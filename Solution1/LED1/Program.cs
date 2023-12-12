1280876a05c0c72d3c8b2f55eca590ad0f179d90
Sewaboy987 <91268771+wi-fiman@users.noreply.github.com>
2023-12-12 16:57:48 +0300

    Попытка добавить мигание светодиода

---
+++ Solution1/LED1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Device.Gpio;
+
+var ledPin = 24;
+var ledOnTime  = 1000;
+var ledOffTime = 1000;
+
+using GpioController controller = new GpioController();
+controller.OpenPin(ledPin, PinMode.Output);
+
+Console.CancelKeyPress += (s, e) => {controller.Dispose();};
+
+while (true)
+{
+    controller.Write(ledPin, PinValue.High);
+    Thread.Sleep(ledOnTime);
+
+    controller.Write(ledPin, PinValue.Low);
+    Thread.Sleep(ledOffTime);
+}
