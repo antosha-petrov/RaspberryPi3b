dfa36310e9b5563d95232fb84786fdbd337fbcee
Sewaboy987 <91268771+wi-fiman@users.noreply.github.com>
2023-12-19 16:26:34 +0300

    добавленна осинхронная работа в проект lightsensor

--- Source/lightsensor/Program.cs
+++ Source/lightsensor/Program.cs
@@ -1,4 +1,55 @@
-﻿using System;
+﻿using CommonUtils;
+using System.Device.Gpio;
+
+using var lifetimeManager = new ConsoleAppLifetimeManager();
+using var cts = new CancellationTokenSource();
+using var controller = new GpioController();
+
+var blinkingTask = BlinkSensorAsync(controller, cts.Token);
+
+await lifetimeManager.WaitForShutdownAsync()
+    .ConfigureAwait(false);
+
+cts.Cancel();
+
+blinkingTask.Wait();
+
+static async Task BlinkSensorAsync(GpioController controller, CancellationToken cancellationToken)
+{
+    var pinAO = 17;
+    var pinDO = 27;
+
+    controller.OpenPin(pinAO, PinMode.Input);
+    controller.OpenPin(pinDO, PinMode.Input);
+
+    while (!cancellationToken.IsCancellationRequested)
+    {
+        try
+        {
+            controller.RegisterCallbackForPinValueChangedEvent(pinDO, PinEventTypes.Rising | PinEventTypes.Falling, LightSensorTriggered);
+
+            Console.ReadLine();
+
+            static void LightSensorTriggered(object sender, PinValueChangedEventArgs args)
+            {
+                Console.WriteLine(args.ChangeType);
+
+            }
+        }
+        catch (OperationCanceledException)
+        {
+        }
+        finally
+        {
+        }
+    }
+}
+
+
+
+
+
+/*using System;
 using System.Threading;
 using Microsoft.VisualBasic;
 using System.Device.Gpio;
@@ -17,4 +68,4 @@ Console.ReadLine();
 static void LightSensorTriggered(object sender, PinValueChangedEventArgs args)
 {
     Console.WriteLine(args.ChangeType);
-}
+}*/
