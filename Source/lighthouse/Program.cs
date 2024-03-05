5758a45c7853bb44d9d0da0a468259e79193c5f5
Sewaboy987 <91268771+wi-fiman@users.noreply.github.com>
2024-03-05 16:59:56 +0300

    Примитивная защита от дрожания, наддо доделать

--- Source/lighthouse/Program.cs
+++ Source/lighthouse/Program.cs
@@ -1,14 +1,9 @@
-﻿/*
-https://github.com/dotnet/iot/blob/main/src/devices/Ads1115/README.md
-https://learn.microsoft.com/ru-ru/dotnet/api/iot.device.ads1115.ads1115?view=iot-dotnet-latest
-*/
-// set I2C bus ID: 1
-// ADS1115 Addr Pin connect to GND
-using CommonUtils;
+﻿using CommonUtils;
 using Iot.Device.Ads1115;
 using lighthouse;
 using System.Device.Gpio;
 using System.Device.I2c;
+using System.Timers;
 
 Console.Clear();
 
@@ -28,50 +23,81 @@ await lifetimeManager.WaitForShutdownAsync()
 cts.Cancel();
 
 blinkingTask.Wait();
-
 static async Task BlinkSensorAsync(GpioController controller, Ads1115 ads, CancellationToken cancellationToken)
 {
     var pinAO = 17;
     var pinDO = 27;
     const int ledPin = 24;
+    bool canContiune = true;
+    int sleepingTime = 1250;
+
+
+    Console.WriteLine("Введите режим работы dayli/short или интервал реагирования в миллесекундах // по умолчанию short");
+    string enter = Console.ReadLine();
+    if (enter == "dayli")
+    {
+        sleepingTime = 600000;
+    }
+    else if (enter == "short")
+    {
+        sleepingTime = 2000;
+    }
+    else
+    {
+        sleepingTime = Convert.ToInt32(enter);
+    }
+
+    System.Timers.Timer timer = new System.Timers.Timer(sleepingTime);
+    timer.Elapsed += TimerElapsed;
 
     controller.OpenPin(pinAO, PinMode.Input);
     controller.OpenPin(pinDO, PinMode.Input);
     controller.OpenPin(ledPin, PinMode.Output);
 
+    controller.Write(ledPin, PinValue.Low);
+
     var reader = new LightSensorReader(controller, pinDO);
+
+    
 
     while (!cancellationToken.IsCancellationRequested)
     {
         try
         {
-            var changeType = await reader.WaitForValueChanging(cancellationToken);
-
-            if (changeType == PinEventTypes.Rising)
+            if (canContiune)
             {
-                controller.Write(ledPin, PinValue.High);
-                Console.WriteLine("lighting on! Success!");
-            }
-            else
-            {
-                controller.Write(ledPin, PinValue.Low);
-                Console.WriteLine("lighting off! Success!");
-            }
+                var changeType = await reader.WaitForValueChanging(cancellationToken);
 
-            // read raw data form the sensor
-            short raw = ads.ReadRaw();
+                short raw = ads.ReadRaw();
+                var voltage = ads.RawToVoltage(raw);
 
-            // raw data convert to voltage
-            var voltage = ads.RawToVoltage(raw);
+                if (changeType == PinEventTypes.Rising)
+                {
+                    controller.Write(ledPin, PinValue.High);
+                    Console.WriteLine("lighting on! Success!");
+                    canContiune = false;
+                }
+                else
+                {
+                    controller.Write(ledPin, PinValue.Low);
+                    Console.WriteLine("lighting off! Success!");
+                    canContiune = false;
+                }
 
-            Console.WriteLine($"Voltage: {voltage.Value} {voltage.Unit}");
+                Console.WriteLine($"Voltage: {voltage.Value} {voltage.Unit}");
+
+                timer.Start();
+            }  
         }
         catch (OperationCanceledException)
-        {
-        }
-        finally
         {
             controller.Write(ledPin, PinValue.Low);
         }
     }
+
+    void TimerElapsed(object? sender, ElapsedEventArgs e)
+    {
+        canContiune = true;
+    }
 }
+
